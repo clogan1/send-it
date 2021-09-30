da2337ca5bdf9cd731bab0c238b9d1cc7ec173e3
Claire <clairelogan16@gmail.com>
2021-09-30 09:00:01 -0700

    Fixed credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,8 +119,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => "senditcardco@gmail.com",
-    :password             => "yjzynvsuwyjxjkgc",
+    :user_name            => Rails.application.credentials.gmail[:username],
+    :password             => Rails.application.credentials.gmail[:password],
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
